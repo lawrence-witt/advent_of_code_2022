3204664778ef635e6290ff125f8db5c23171d9f1
Lawrence Witt <lawrence.witt@hotmail.co.uk>
2022-12-04 14:58:21 +0000

    Day 3 Part 1

---
+++ day-03/part-1.hs
@@ -0,0 +1,18 @@
+import Data.Char (ord, isUpper)
+
+main = do
+    s <- readFile "input.txt"
+    print(processLines (lines s) 0)
+
+get_duplicate :: String -> String -> Char
+get_duplicate (x:xs) y = if x `elem` y then x else get_duplicate xs y
+
+get_score :: Char -> Int
+get_score x = if isUpper x then (ord x) - 38 else (ord x) - 96
+
+processLines :: [String] -> Int -> Int
+processLines [] y = y
+processLines (x:xs) y = processLines xs t
+    where 
+        (s, e) = splitAt ((length x) `div` 2) x
+        t = y + get_score (get_duplicate s e)
