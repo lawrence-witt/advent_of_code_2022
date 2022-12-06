aad679ac668d37fa4ec08d8ac525deedef4105b6
Lawrence Witt <lawrence.witt@hotmail.co.uk>
2022-12-06 23:12:16 +0000

    Encode download prompt

--- day-06/deploy.sh
+++ day-06/deploy.sh
@@ -1,5 +1,4 @@
-# To run this script you must first install GoChain Web3: https://github.com/gochain/web3
-
+web3 1> /dev/null || echo "You must install GoChain Web3 to run this script: https://github.com/gochain/web3"
 docker stop gochain 1> /dev/null && docker rm gochain 1> /dev/null
 echo "Starting node..."
 export WEB3_PRIVATE_KEY=$(web3 start 2> /dev/null | awk '$1 ~ /^(0x.+$)/ {print $0}')
@@ -8,6 +7,7 @@ while ! web3 id 2> /dev/null; do
     echo "Awaiting node connection..."
     sleep 3
 done
+echo "Deploying contract..."
 export CONTRACT_ADDRESS=$(web3 contract deploy Contract.bin | awk 'FNR==2{print $4}')
 echo "Solving for a slice size of $1..."
 web3 contract call --address $CONTRACT_ADDRESS --abi Contract.abi --function solve $(cat input.txt) $1
