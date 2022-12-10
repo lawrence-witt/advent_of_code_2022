9dec4f7379b6d911e67f55c7b9a35224c26e6d40
Lawrence Witt <lawrence.witt@hotmail.co.uk>
2022-12-10 19:04:36 +0000

    Clean up day 10

--- day-10/script.c
+++ day-10/script.c
@@ -12,7 +12,17 @@ int signal_product(int cycles, int x) {
     return 0;
 }
 
-int main() {
+void assign_pixel(int cycles, int x, char display[6][40]) {
+    int row = cycles % 40;
+    int col = cycles - (row * 40);
+    if (row >= x - 1 && row <= x + 1) {
+        display[row][col] = '#';
+    } else {
+        display[row][col] = '.';
+    }
+}
+
+int main(int argc, char **argv) {
     FILE* file_pointer;
     char buffer[BUFFER_LEN];
 
@@ -28,28 +38,50 @@ int main() {
         printf("Could not compile regular expression\n");
         return 1;
     }
-
+    
     int cycles = 0;
     int x = 1;
-    int total = 0;
+    int signal_total = 0;
+    char display[6][40] = {};
+
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 40; j++) {
+            display[i][j] = ' ';
+        }
+    }
 
     while (fgets(buffer, BUFFER_LEN, file_pointer)) {
         buffer[strcspn(buffer, "\n")] = 0;
         regmatch_t groups[1];
         if (regexec(&expr, buffer, 1, groups, 0) == 0) {
             int i = atoi(buffer + groups[0].rm_so);
+            assign_pixel(cycles, x, display);
             cycles++;
-            total += signal_product(cycles, x);
+            signal_total += signal_product(cycles, x);
+            assign_pixel(cycles, x, display);
             cycles++;
-            total += signal_product(cycles, x);
+            signal_total += signal_product(cycles, x);
             x += i;
         } else {
+            assign_pixel(cycles, x, display);
             cycles++;
-            total += signal_product(cycles, x);
+            signal_total += signal_product(cycles, x);
         }
     }
 
-    printf("%i", total);
+    if (*argv[1] == '1') {
+        printf("%i", signal_total);
+    }
+    
+    if (*argv[1] == '2') {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 40; j++) {
+                char c = display[i][j];
+                putchar(c);
+            }
+            printf("\n");
+        }
+    }
 
     regfree(&expr);
     fclose(file_pointer);
