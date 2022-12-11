e41edd0c7b9021d576a8180073bcf7062b38f6fa
Lawrence Witt <lawrence.witt@hotmail.co.uk>
2022-12-11 19:43:59 +0000

    Complete day 11

--- day-11/monkey.py
+++ day-11/monkey.py
@@ -1,4 +1,4 @@
-import math
+import operator
 
 class Monkey:
     def __init__(self, items, operation, test):
@@ -10,12 +10,13 @@ class Monkey:
     def throw(self, item):
         self.items.append(item)
 
-    def inspect(self):
-        self.inspections += 1
-        self.items[0] = math.floor(self.operation(self.items[0]) / 3)
-
-    def round(self, monkeys):
+    def round(self, monkeys, lcm=None):
         while len(self.items) > 0:
-            self.inspect()
+            self.inspections += 1
+            self.items[0] = self.operation(self.items[0])
+            if lcm:
+                self.items[0] %= lcm
+            else:
+                self.items[0] = operator.floordiv(self.items[0], 3)
             target = self.test(self.items[0])
             monkeys[target].throw(self.items.pop(0))
--- day-11/script.py
+++ day-11/script.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+import sys
 import re
 import operator
+import math
 from monkey import Monkey
 
 def operation_factory(operator, operand):
@@ -9,25 +11,31 @@ def operation_factory(operator, operand):
 def division_factory(divisor, m1, m2):
     return lambda x: m1 if x % divisor == 0 else m2
 
+def parse_digits(string):
+    return int(re.search(r"(\d+)", string).group(1))
+
 file = Path('input.txt').read_text()
 ops = {"+": operator.add, "*": operator.mul}
 
 monkeys = []
+divisors = []
 
 for obj in file.split("\n\n"):
     fields = obj.split("\n")
     items = list(map(lambda x: int(x), re.findall(r"(\d+)", fields[1])))
     op_list = re.findall(r"([*|+])\s(\d+|old)", fields[2])
     operation = operation_factory(ops[op_list[0][0]], op_list[0][1])
-    divisor = int(re.search(r"(\d+)", fields[3]).group(1))
-    m1 = int(re.search(r"(\d+)", fields[4]).group(1))
-    m2 = int(re.search(r"(\d+)", fields[5]).group(1))
-    test = division_factory(divisor, m1, m2)
+    divisor = parse_digits(fields[3])
+    test = division_factory(divisor, parse_digits(fields[4]), parse_digits(fields[5]))
     monkeys.append(Monkey(items, operation, test))
+    divisors.append(divisor)
 
-for i in range(20):
-    for j in range(len(monkeys)):
-        monkeys[j].round(monkeys)
+rounds = 20 if sys.argv[1] == "1" else 10000
+lcm = None if sys.argv[1] == "1" else math.lcm(*divisors)
+
+for i in range(rounds):
+    for m in monkeys:
+        m.round(monkeys, lcm)
 
 monkeys.sort(reverse=True, key=lambda x: x.inspections)
 
